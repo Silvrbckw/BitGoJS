dbbba01e161c96180ade4554d90ed2bda9692738
Adarsh Ravichandran <adarshravichandran@bitgo.com>
2024-08-07 14:55:42 +0530

    feat(sdk-coin-bera): add bera sdk skeleton TICKET: WIN-3180

--- modules/statics/src/networks.ts
+++ modules/statics/src/networks.ts
@@ -1018,6 +1018,20 @@ class BeraTestnet extends Testnet implements EthereumNetwork {
   walletImplementationAddress = '0xe5301a9c2131d4A9b137a1fa8eee9436dda74f5d';
 }
 
+class BaseChainTestnet extends Testnet implements EthereumNetwork {
+  name = 'BaseChainTestnet';
+  family = CoinFamily.BASEETH;
+  explorerUrl = 'https://sepolia.basescan.org/tx/';
+  accountExplorerUrl = 'https://sepolia.basescan.org/address/';
+  chainId = 84532;
+  nativeCoinOperationHashPrefix = '84532';
+  tokenOperationHashPrefix = '84532-ERC20';
+  forwarderFactoryAddress = '0xdd498702f44c4da08eb9e08d3f015eefe5cb71fc';
+  forwarderImplementationAddress = '0xbe69cae311191fb45e648ed20847f06fad2dbab4';
+  walletFactoryAddress = '0x4550e1e7616d3364877fc6c9324938dab678621a';
+  walletImplementationAddress = '0x92db2759d1dca129a0d9d46877f361be819184c4';
+}
+
 export const Networks = {
   main: {
     ada: Object.freeze(new Ada()),
